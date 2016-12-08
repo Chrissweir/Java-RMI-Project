e10c4d236a4c77688b0009d70a915471e9ff992a
Christopher Weir <christopher.weir@hotmail.com>
2016-12-08 17:03:12 +0000

    Updates

--- src/ie/gmit/sw/ServiceHandler.java
+++ src/ie/gmit/sw/ServiceHandler.java
@@ -48,18 +48,18 @@ public class ServiceHandler extends HttpServlet {
 		out.print("Note that any variables declared inside this doGet() method are thread safe. Anything defined at a class level is shared between HTTP requests.");				
 		out.print("</b></font>");
 
-		out.print("<P> Next Steps:");	
+		/*out.print("<P> Next Steps:");	
 		out.print("<OL>");
 		out.print("<LI>Generate a big random number to use a a job number, or just increment a static long variable declared at a class level, e.g. jobNumber.");	
 		out.print("<LI>Create some type of an object from the request variables and jobNumber.");	
 		out.print("<LI>Add the message request object to a LinkedList or BlockingQueue (the IN-queue)");			
-		//out.print("<LI>Return the jobNumber to the client web browser with a wait interval using <meta http-equiv=\"refresh\" content=\"10\">. The content=\"10\" will wait for 10s.");	
+		out.print("<LI>Return the jobNumber to the client web browser with a wait interval using <meta http-equiv=\"refresh\" content=\"10\">. The content=\"10\" will wait for 10s.");	
 		out.print("<LI>Have some process check the LinkedList or BlockingQueue for message requests.");	
 		out.print("<LI>Poll a message request from the front of the queue and make an RMI call to the String Comparison Service.");			
 		out.print("<LI>Get the <i>Resultator</i> (a stub that is returned IMMEDIATELY by the remote method) and add it to a Map (the OUT-queue) using the jobNumber as the key and the <i>Resultator</i> as a value.");	
 		out.print("<LI>Return the result of the string comparison to the client next time a request for the jobNumber is received and the <i>Resultator</i> returns true for the method <i>isComplete().</i>");	
 		out.print("</OL>");	
-		
+		*/
 		out.print("<form name=\"frmRequestDetails\">");
 		out.print("<input name=\"cmbAlgorithm\" type=\"hidden\" value=\"" + algorithm + "\">");
 		out.print("<input name=\"txtS\" type=\"hidden\" value=\"" + s + "\">");
--- src/ie/gmit/sw/StringServiceImpl.java
+++ src/ie/gmit/sw/StringServiceImpl.java
@@ -6,16 +6,17 @@ import java.rmi.server.UnicastRemoteObject;
 public class StringServiceImpl extends UnicastRemoteObject implements StringService{
 
 	private static final long serialVersionUID = 1L;
+	private Resultator result;
 	
 	protected StringServiceImpl() throws RemoteException {
-		super();
 	}
-
-	
 
 	@Override
 	public Resultator compare(String s, String t, String algo) throws RemoteException {
-		return null;
+
+		 result = new ResultatorImpl();
+		 AlgoComparator compareJob = new AlgoComparator(s, t, result, algo);
+		 return null;
 	}
 
 
